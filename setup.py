e1937e643dc8850c5b29b0f275497081840465ab
tiborauer <tibor.auer@gmail.com>
2019-01-26 23:37:16 +0000

    VERSION

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=['pyniexp'],
     install_requires=['keyboard','nidaqmx'],
     
-    version='0.1',
+    version='0.2',
     license='GPL-3.0',
     description='Python interface for neuroimaging experiments',
     
