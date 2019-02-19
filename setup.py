fc184e7b98898b68e17cf9ee885c15de44bc4f28
MRI <tibor.auer@gmail.com>
2019-02-19 16:35:23 +0000

    VERSION

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=['pyniexp'],
     install_requires=['keyboard','nidaqmx'],
     
-    version='0.7.1',
+    version='0.7.2',
     license='GPL-3.0',
     description='Python interface for neuroimaging experiments',
     
