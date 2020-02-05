b726436f4aef219ffad26165d0372ad663300118
tiborauer <tibor.auer@gmail.com>
2020-02-05 12:30:06 +0000

    UPDATE: mlplugin classes for data and image

--- pyniexp/mlplugins.py
+++ pyniexp/mlplugins.py
@@ -1,4 +1,4 @@
-import numpy as np
+from numpy import array, prod, flip
 from multiprocessing import Process, Value, RawArray
 from loguru import logger
 
@@ -7,13 +7,11 @@ SIG_RUNNING = 1
 SIG_STOPPED = 0
 SIG_NEWIMAGE = 10
 
-class imageProcess:
+class dataProcess:
     __process = Process()
-    _image_dimension = None
 
-    def __init__(self,image_dimension,autostart=True):
-        self._image_dimension = image_dimension
-        self._buffer = RawArray('d',[0]*np.prod(self._image_dimension))
+    def __init__(self,data_size,autostart=True):
+        self._buffer = RawArray('d',[0]*data_size)
         self._signal = Value('b',SIG_NOTSTARTED)
         if autostart: self.start_process()
 
@@ -23,8 +21,8 @@ class imageProcess:
             self._signal.value = SIG_STOPPED
 
     # Processing
-    def process(self,image):
-        """Image processing method to overwrite"""
+    def process(self,data):
+        """Data processing method to overwrite"""
         return NotImplemented
     
     # Mechanism
@@ -33,12 +31,12 @@ class imageProcess:
         self.__process = Process(target=self._run)
         self.__process.start()
 
-    def load_image(self,mlImage):
+    def load_data(self,mlData):
         if not(self.__process.is_alive()):
             logger.exception('Process is not running')
             return
 
-        for l in range(0,len(self._buffer)): self._buffer[l] = mlImage[l]
+        for l in range(0,len(self._buffer)): self._buffer[l] = mlData[l]
         self._signal.value = SIG_NEWIMAGE
     
     def _run(self):
@@ -47,7 +45,25 @@ class imageProcess:
                 logger.info('Process is running')
                 self._signal.value = SIG_RUNNING
             if self._signal.value == SIG_NEWIMAGE:
-                img = np.array(self._buffer).reshape(np.flip(self._image_dimension,0)).transpose(2,1,0) 
+                logger.info('New data')
+                self.process(self._buffer)
+                self._signal.value = SIG_RUNNING
+        logger.info('Process is stopped')
+
+class imageProcess(dataProcess):
+    _image_dimension = None
+
+    def __init__(self,image_dimension,autostart=True):
+        self._image_dimension = image_dimension
+        super().__init__(prod(self._image_dimension),autostart)
+    
+    def _run(self):
+        while self._signal.value != SIG_STOPPED:
+            if self._signal.value == SIG_NOTSTARTED: 
+                logger.info('Process is running')
+                self._signal.value = SIG_RUNNING
+            if self._signal.value == SIG_NEWIMAGE:
+                img = array(self._buffer).reshape(flip(self._image_dimension,0)).transpose(2,1,0) 
                 logger.info('New image')
                 self.process(img)
                 self._signal.value = SIG_RUNNING
