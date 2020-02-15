46a083c623ac03dc7b9a978e8d99aaa639929874
tiborauer <tibor.auer@gmail.com>
2020-02-15 11:38:29 +0000

    UPDATE: reorganise examples

---
+++ examples/example_stimulation.py
@@ -0,0 +1,18 @@
+from pyniexp.stimulation import Waveform, Stimulator
+
+stim = Stimulator()
+
+# Output Signal Parameters
+phase=0
+
+wave1 = Waveform(amplitude = 1, frequency = 10, phase=0, duration = 20, rampUp = 4, rampDown = 4, samplingRate = 1000)
+wave2 = Waveform(amplitude = 1, frequency = 10, phase=phase, duration = 20, rampUp = 4, rampDown = 4, samplingRate = 1000)
+
+# wave1.show()
+# wave2.show()
+
+stim.loadWaveform([wave1, wave2])
+
+stim.stimulate()
+
+stim = None
--- pyniexp/stimulation.py
+++ pyniexp/stimulation.py
@@ -126,24 +126,3 @@ class Stimulator:
     
     def stimulate(self):
         self._DAQ.start()
-    
-"""
-from pyES import Waveform, Stimulator
-
-stim = Stimulator()
-
-# Output Signal Parameters
-phase=0
-
-wave1 = Waveform(amplitude = 1, frequency = 10, phase=0, duration = 20, rampUp = 4, rampDown = 4, samplingRate = 1000)
-wave2 = Waveform(amplitude = 1, frequency = 10, phase=phase, duration = 20, rampUp = 4, rampDown = 4, samplingRate = 1000)
-
-# wave1.show()
-# wave2.show()
-
-stim.loadWaveform([wave1, wave2])
-
-stim.stimulate()
-
-stim = None
-"""
