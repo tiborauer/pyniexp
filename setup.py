8e723c4a3d66181e0c9208e0ae7705aa1535df73
unknown <nemo@PSL014.surrey.ac.uk>
2020-02-18 14:16:55 +0000

    VERSION

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     packages=['pyniexp'],
     install_requires=['keyboard','nidaqmx','loguru','matplotlib'],
     
-    version='0.20.0',
+    version='0.20.1',
     license='GPL-3.0',
     description='Python interface for neuroimaging experiments',
     
