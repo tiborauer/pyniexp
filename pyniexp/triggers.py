acc5e74879d55b8ed5aaf8a7a3e2c90be3e81c68
tiborauer <tibor.auer@gmail.com>
2020-12-01 08:55:53 +0000

    NEW: triggers (for BV)

---
+++ pyniexp/triggers.py
@@ -0,0 +1,32 @@
+import serial
+from loguru import logger
+
+class BrainVision:
+    _port = []
+    _isConnected = False
+
+    def isConnected(self):
+        return self._isConnected
+
+    def __init__(self,port='COM6'):
+        if not any([p.device == port for p in serial.tools.list_ports.comports()]):
+            logger.error('port {} is not available'.format(port))
+        else:
+            self._port = serial.Serial(port)
+            self._port.write([0x00])
+            self._isConnected = True
+    
+    def __del__(self):
+        if self.isConnected: 
+            self.close()
+
+    def close(self):
+        self._port.write([0xFF])
+        self._port.close()
+    
+    def send(self,val):
+        if val > 255:
+            logger.error('trigger value {:d} is larger than 255'.format(val))
+        else:
+            self._port.write([val])
+            self._port.write([0x00])
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     package_data={'pyniexp': ['stimulatordlg.ui']},
     include_package_data=True,
 
-    version='0.26.2',
+    version='0.27.0',
     license='GPL-3.0',
     description='Python interface for neuroimaging experiments',
     
