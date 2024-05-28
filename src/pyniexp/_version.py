32e7bd91a25c451d54a8b10980f8c84bcfc63511
tiborauer <tibor.auer@gmail.com>
2024-05-28 18:31:38 +0100

    VERSION

--- src/pyniexp/_version.py
+++ src/pyniexp/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.13.11.dev53+g19aaaf4.d20240528'
-__version_tuple__ = version_tuple = (0, 13, 11, 'dev53', 'g19aaaf4.d20240528')
+__version__ = version = '0.28.dev0'
+__version_tuple__ = version_tuple = (0, 28, 'dev0')
